0815d81f8243ec5ef2483350447915b11ae2c03f
torres2014et <torres2014.et@gmail.com>
2025-10-04 01:03:59 -0500

    Delete ejercicio-4.py

--- ejercicio-4.py
+++
@@ -1,9 +0,0 @@
-def run(cadena):
-    """
-    EJERCICIO 4: Reconocer a^n b^n c^n
-    Ejemplo: aaabbbccc
-    """
-    n = len(cadena) // 3
-    if cadena == "a" * n + "b" * n + "c" * n and n > 0:
-        return cadena, True
-    return cadena, False
